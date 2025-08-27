1c3127034284279577088079142537a9e19f0265
abdul-rahman18 <mohammedabdulrahman188@gmail.com>
2025-08-27 11:30:19 +0530

    Initial commits

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # LeetcodeMetrics
-This site allows you to fetch a users complete leetcode profile.
+
+### What is Leetcode Metrics ?
+Leetcode Metrics is a site that allows you to fetch crutial info about a leetcode data , such as their progress so far.
+
+You can visit this wbesite here : 
+
+### What does it actually show ?
+It shows 7 crutial thing you need to know about an user, they are : 
+- Overall submission of an user.
+- Overall Easy Submissions.
+- Overall Medium Submissions.
+- Overall Hard Submissions.
+- Number of Easy questions attempted.
+- Number of Medium questions attempted.
+- Number of Hard questions attempted.
+
+### How does this work ?
+Steps to make this work :
+1. Get the username from user input.
+2. Validate it
+    - To validate it get the regex expressions for leetcode usernames.
+    #### it should be something like : 
+    ```const regex = /^[a-zA-Z0-9_-]{1,15}$/;```
+3. Once you validate it now , you have to fetch user details.
+    - We can fetch user profile from api end point : https://leetcode.com/graphql/ , just write a simple query to fetch it.
+    ```const graphql = JSON.stringify({
+                query: "\n    query userSessionProgress($username: String!) {\n  allQuestionsCount {\n    difficulty\n    count\n  }\n  matchedUser(username: $username) {\n    submitStats {\n      acSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n      totalSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n    }\n  }\n}\n    ",
+                variables: { "username": `${username}` }
+        });```
+4. But there's a catch , leetcode does not direct allow me to fetch it, so I used a proxy server to fetch it.
+    - what it does is , it takes your url something like this : 
+        - https://cors-anywhere.herokuapp.com/ + (your url)
+    - Then process your url.
+    - I found this on internet on stack overflow : https://stackoverflow.com/questions/29670703/how-to-use-cors-anywhere-to-reverse-proxy-and-add-cors-headers
+5. Now , you have all the data in the form JSON.
+6. We can simply work with it and display it to the user.
+
+### My key learning : 
+I learned :
+- How async await actually works.
+- How to fetch an API.
+- How POST works.
+- Learned about proxy servers.
+- How to work with JSON.
+- Learned error handling.
+- and much more...
+
+If you face an issue , something like , No data found , that is due to proxy server. Just to this website : https://cors-anywhere.herokuapp.com/corsdemo and hit "Request temporary access to the demo server" , then wait for few second and it should work fine.
+
+### Output images / website preview : 
+
+![](./images/op1.png)
+> user on leetcode website
+![](./images/op2.png)
+> user's details
+![](./images/op3.png)
+> My profile
+
+### Images during development : 
+
+![](./images/d1.png)
+![](./images/d2.png)
+![](./images/d3.png)
+![](./images/d4.png)
---
+++ script.js
@@ -0,0 +1,130 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    
+    const usernameInput = document.getElementById("user-input");
+    const search = document.getElementById("search-button");
+    const stats = document.querySelector(".stats");
+    const easyCircle = document.querySelector(".easy-progress");
+    const mediumCircle = document.querySelector(".medium-progress");
+    const hardCircle = document.querySelector(".hard-progress");
+    const easyLabel = document.getElementById("easy-label");
+    const mediumLabel = document.getElementById("medium-label");
+    const hardLabel = document.getElementById("hard-label");
+    const cardStats = document.querySelector(".stats-card");
+
+
+    function validateUsername(username)
+    {
+        if(username.trim() === "")
+        {
+            alert("Username should not be empty.");
+            return false;
+        }
+        const regex = /^[a-zA-Z0-9_-]{1,15}$/;
+        const isMatching = regex.test(username);
+        if(!isMatching) alert("Invalid Username");
+        return isMatching;
+    }
+
+
+    async function fetchUserDetails(username) {
+
+        try{
+            search.textContent = "Searching...";
+            search.disabled = true;
+
+            const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+
+        const targetUrl = 'https://leetcode.com/graphql/';
+        const header = new Headers();
+        header.append("content-type","application/json");
+
+        const graphql = JSON.stringify({
+                query: "\n    query userSessionProgress($username: String!) {\n  allQuestionsCount {\n    difficulty\n    count\n  }\n  matchedUser(username: $username) {\n    submitStats {\n      acSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n      totalSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n    }\n  }\n}\n    ",
+                variables: { "username": `${username}` }
+        });
+
+        const requestOptions = {
+            method: "POST",
+            headers: header,
+            body: graphql,
+            redirect: "follow"
+        };
+
+        const response = await fetch((proxyUrl+targetUrl),requestOptions);
+            if(!response.ok){
+                throw new Error("Unable to fetch the user details");
+            }
+
+            const parsedData = await response.json();
+            console.log(parsedData);
+
+            displayData(parsedData);
+        }
+        catch(err){
+            stats.innerHTML = 
+                `<p>No data Found</p>`;
+        }
+        finally{
+            search.textContent = "Search";
+            search.disabled = false;
+        }
+    }
+
+    function updateProgress(solved,total,label,circle)
+    {
+        const pd = (solved/total)*100;
+        circle.style.setProperty("--progress-degree",`${pd}%`);
+        label.textContent = `${solved}/${total}`;
+
+
+    }
+
+
+    function displayData(parsedData)
+    {
+        const totalQuestions = parsedData.data.allQuestionsCount[0].count;
+        const totalEasyQuestions = parsedData.data.allQuestionsCount[1].count;
+        const totalMediumQuestions = parsedData.data.allQuestionsCount[2].count;
+        const totalHardQuestions = parsedData.data.allQuestionsCount[3].count;
+
+        const solvedTotalQuestions = parsedData.data.matchedUser.submitStats.acSubmissionNum[0].count;
+        const solvedEasyTotalQuestions = parsedData.data.matchedUser.submitStats.acSubmissionNum[1].count;
+        const solvedMediumTotalQuestions = parsedData.data.matchedUser.submitStats.acSubmissionNum[2].count;
+        const solvedHardTotalQuestions = parsedData.data.matchedUser.submitStats.acSubmissionNum[3].count;
+
+        updateProgress(solvedEasyTotalQuestions,totalEasyQuestions,easyLabel,easyCircle);
+        updateProgress(solvedMediumTotalQuestions,totalMediumQuestions,mediumLabel,mediumCircle);
+        updateProgress(solvedHardTotalQuestions,totalHardQuestions,hardLabel,hardCircle);
+
+
+
+        const cardsData = [
+        {label:"Overall Submissions",value:parsedData.data.matchedUser.submitStats.totalSubmissionNum[0].submissions},
+        {label:"Overall Easy Submissions",value:parsedData.data.matchedUser.submitStats.totalSubmissionNum[1].submissions},
+        {label:"Overall Medium Submissions",value:parsedData.data.matchedUser.submitStats.totalSubmissionNum[2].submissions},
+        {label:"Overall Hard Submissions",value:parsedData.data.matchedUser.submitStats.totalSubmissionNum[3].submissions}
+        ];
+
+        console.log("CardData : " , cardsData);
+
+        cardStats.innerHTML = cardsData.map(
+            data => {
+                return `
+                    <div class="card">
+                    <h4>${data.label}</h4>
+                    <p>${data.value}</p>
+                    </div>
+                `
+            }
+        ).join("");
+    }
+
+    search.addEventListener("click",()=>{
+        const username = usernameInput.value;
+        if(validateUsername(username))
+        {
+            fetchUserDetails(username);
+        }
+    });
+
+});
